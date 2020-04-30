60be73bbfd585677b4bdb47c2a2b04edd386a961
deb <lee025@gmail.com>
2020-04-30 16:06:05 -0400

    News API added

--- src/components/News.vue
+++ src/components/News.vue
@@ -5,7 +5,26 @@
 </template>
 
 <script>
+import axios from 'axios'
 export default {
+  name: 'News',
+  data () {
+    return {
+      apiKey: '3541fbfb60064ba4a89453bfdb61f3c9',
+      articles: [],
+      errors: []
+    }
+  },
+  created () {
+    axios.get('http://newsapi.org/v2/everything?domains=wsj.com&apiKey=' + this.apiKey)
+      .then(res => {
+        this.articles = res.data.articles
+        console.log('data:', res.data.articles)
+      })
+      .catch(error => {
+        this.errors.push(error)
+      })
+  }
 }
 </script>
 
